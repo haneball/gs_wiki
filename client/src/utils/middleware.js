ac2a555afc54f0fc83c037a53b1312e2d2bb7908
haneball <dendro@yeah.net>
2023-09-11 12:58:08 +0800

    remove useless code

--- client/src/utils/middleware.js
+++ client/src/utils/middleware.js
@@ -10,7 +10,6 @@ export function debounce (callbackFn, delay, immediate = false) {
   let done = false
 
   return function _debounce (...args) {
-    console.log('ok, ' + delay)
     if (timer) {
       clearTimeout(timer)
     }
@@ -19,9 +18,7 @@ export function debounce (callbackFn, delay, immediate = false) {
       callbackFn.apply(this, args)
       done = true
     } else {
-      console.log('else')
       timer = setTimeout(() => {
-        console.log('setTimeout')
         callbackFn.apply(this, args)
       }, delay)
       done = false
